83122d8930bf26959e62aaf612e8efabb63d0529
ca <91000254+CA-SKGC@users.noreply.github.com>
2021-10-05 21:22:51 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php include 'include/header.php'; ?>
+
+<?php include 'include/slider.php'; ?>
+
+
+
+<?php include 'include/home-page-whoWeAre.php'; ?>
+
+<?php include 'include/home-page-service.php'; ?>
+
+
+
+<?php include 'include/footer.php'; ?>
