3fcc4c4d27ae16ad670a19ef8e21b19af36c30ea
ca <91000254+CA-SKGC@users.noreply.github.com>
2021-10-05 21:26:29 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,115 @@
+<?php include 'include/header.php'; ?>
+
+<section class="container">
+  <h1 class="text-center taxt-capital">ABOUT US</h1>
+    <p> <b>Surabh K Grag & Co.</b> is a Chartered Accountancy firm registered with
+      Institute of Chartered Accountants of India (ICAI) headquartered in Faridabad.
+       We are a team of enthusiastic and professional partners with an aim to offer
+       finest accountancy services to our clients. We have a positive approach towards
+       providing expert services following professional ethics. Our practice of offering
+       high quality professional services has been a great help for us in building strong
+        long term mutual beneficial relationships with our clients. We have now become a priority
+       accountancy service provider with our quick and reliable services. Our services
+       portfolio includes income tax consultancy, accounting, auditing, taxation,
+       certification, business formation, consultancy services, Company incorporation,
+       business development, NGO/Trust etc.</p>
+</section>
+
+<!--          our mission -->
+
+  <section class="container mt-4 our mission">
+    <h1 class="text-center">our mission</h1>
+    <div class="row">
+      <div class="col-xm-12 col-sm-12 col-md-6 col-lx-6 ">
+        <section class="row home-about-picture">
+            <figure class="col-6">
+                  <img src="img/about-1.jpg" alt="" />
+            </figure>
+
+            <figure class="col-6 pt-2">
+
+            <img src="img/about-2.jpg" alt="img-2" />
+            <img src="img/about-3.jpg" alt="img-3" />
+            </figure>
+
+
+        </section>
+      </div>
+
+      <div class="col-xm-12 col-sm-12 col-md-6 col-lx-6 ">
+        <article class="our-mission-article">
+          At Surabh K Grag & Co , we work with a mission to offer best-in-class quality of services
+           with the help of a skilled and expert team of professionals in all the respective fields. We also aim at
+           exceeding client’s expectations and help them lead in their own business.
+           By leveraging our knowledge base, we help our clients realize new opportunities and meet new challenges.
+        </article>
+      </div>
+    </div>
+
+  </section>
+  <!-- our mission end -->
+
+  <!-- our director -->
+  <section class="container">
+    <div class="row">
+      <section class="col-xm-12 col-sm-12 col-md-6 col-lx-6">
+
+      </section>
+
+      <article class="director-article">
+
+      </article>
+
+    </div>
+  </section>
+
+  <!-- our director -->
+  <!-- Request a Free Quote -->
+  <section class="container">
+    <div class="row">
+      <section class="col-xm-12 col-sm-12 col-md-6 col-lx-6">
+        <img src="" alt="image">
+
+      </section>
+
+      <article class="col-xm-12 col-sm-12 col-md-6 col-lx-6">
+
+
+
+        <!-- <form class="" action="" method="post">
+            <div class="row">
+
+          <div class="col-xm-12 col-sm-12 col-md-6 col-lx-6">
+            <label for="">FRIST NAME</label>
+            <input type="text" name="" value="FIRST NAME">
+          </div>
+
+
+
+          <div class="col-xm-12 col-sm-12 col-md-6 col-lx-6">
+            <label for="">LAST NAME</label>
+            <input type="text" name="" value="LAST NAME">
+          </div>
+
+
+
+          <div class="col-xm-12 col-sm-12 col-md-12 col-lx-12">
+            <label for="">Subject </label>
+            <input type="text" name="" value="Subject">
+          </div>
+
+          </div>
+
+        </form> -->
+
+
+      </article>
+
+    </div>
+  </section>
+
+  <!-- end Request a Free Quote -->
+
+
+
+<?php include 'include/footer.php'; ?>
---
+++ services.php
@@ -0,0 +1,82 @@
+<?php include 'include/header.php'; ?>
+
+<section class="container">
+
+  <!-- ///////////////////////////////////////////////////////////////////////////// row 1 ////////////////////////////////////////////////////// -->
+      <div class="row m-4">
+          <div class="col-xm-12 col-sm-12 col-md-3 col-lx-3">
+            <i class="fas fa-scroll"></i>
+            <h1>taxation</h1>
+
+
+          </div>
+
+          <div class="col-xm-12 col-sm-12 col-md-3 col-lx-3">
+
+          </div>
+
+          <div class="col-xm-12 col-sm-12 col-md-3 col-lx-3">
+
+          </div>
+
+          <div class="col-xm-12 col-sm-12 col-md-3 col-lx-3">
+
+          </div>
+      </div>
+  <!-- ///////////////////////////////////////////////////////////////////////////// end row 1 ////////////////////////////////////////////////////// -->
+
+      <!-- ///////////////////////////////////////////////////////////////////////////// row 2 ////////////////////////////////////////////////////// -->
+      <div class="row m-4">
+          <div class="col-xm-12 col-sm-12 col-md-3 col-lx-3">
+
+          </div>
+
+          <div class="col-xm-12 col-sm-12 col-md-3 col-lx-3">
+
+          </div>
+
+          <div class="col-xm-12 col-sm-12 col-md-3 col-lx-3">
+
+          </div>
+
+          <div class="col-xm-12 col-sm-12 col-md-3 col-lx-3">
+
+          </div>
+      </div>
+
+
+
+
+        <!-- ///////////////////////////////////////////////////////////////////////////// end row 2 ////////////////////////////////////////////////////// -->
+
+
+        <!-- ///////////////////////////////////////////////////////////////////////////// row 3 ////////////////////////////////////////////////////// -->
+        <div class="row m-4">
+            <div class="col-xm-12 col-sm-12 col-md-3 col-lx-3">
+
+            </div>
+
+            <div class="col-xm-12 col-sm-12 col-md-3 col-lx-3">
+
+            </div>
+
+            <div class="col-xm-12 col-sm-12 col-md-3 col-lx-3">
+
+            </div>
+
+            <div class="col-xm-12 col-sm-12 col-md-3 col-lx-3">
+
+            </div>
+        </div>
+
+
+
+
+          <!-- ///////////////////////////////////////////////////////////////////////////// end row 3 ////////////////////////////////////////////////////// -->
+
+
+</section>
+
+
+
+<?php include 'include/footer.php'; ?>
